1b0cb67ff4d160624a19d15538d6d9cb1693f408
Neeraja <mnrishi2002@gmail.com>
2022-05-02 09:01:47 +0000

    adding project

---
+++ Conditionals/src/Main.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        int a=5;
+//        int b=10;
+//        if (a>b) {
+//            System.out.println(a);
+//        }
+//        else
+//        {   System.out.println(b);
+//        }
+//        int num = 4;
+//        if (num % 2 == 0){
+//            System.out.println("Number is even");
+//        }
+//        else
+//        {
+//            System.out.println("Number is odd");
+//        }
+
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("Enter the colour of light (R OR G): ");
+//        char colour = scan. nextLine().charAt(0);
+//        System.out.println(colour);
+//        if (colour == 'R')
+//            System.out.println("STOP! \n");
+//        else if (colour == 'G')
+//            System.out.println("GO! \n");
+//        else System.out.println("INVALID COLOUR! \n");
+//        return;
+
+        int ch;
+        System.out.println("Enter any number between 1 & 12...!\n");
+        Scanner r = new Scanner(System.in);
+        ch = r.nextInt();
+        switch(ch) {
+            case 1:
+                System.out.println("winter");
+                break;
+            case 2:
+                System.out.println("winter");
+                break;
+            case 3:
+                System.out.println("spring");
+                break;
+            case 4:
+                System.out.println("spring");
+                break;
+            case 5:
+                System.out.println("spring");
+                break;
+            case 6:
+                System.out.println("spring");
+                break;
+            case 7:
+                System.out.println("summer");
+                break;
+            case 8:
+                System.out.println("summer");
+                break;
+            case 9:
+                System.out.println("summer");
+                break;
+            case 10:
+                System.out.println("summer");
+                break;
+            case 11:
+                System.out.println("winter");
+                break;
+            case 12:
+                System.out.println("winter");
+                break;
+            default:
+                System.out.println("Choice was Invalid");
+        }
+
+        }
+
+        }
+
+
+
+
---
+++ VariablesExercise/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//       String firstName = "Neeraja";
+//        String lastName = "Marri";
+//         String name = firstName + " " +lastName;
+//        System.out.println(name);
+//
+//       int num1 = 2;
+//        int num2 = 4;
+//        int sum = num1 + num2;
+//        system.out.println(sum);
+//
+//       float num1 = 7;
+//        float num2 = 3;
+//        float result = num1 / num2;
+//        System.out.println(result);
+//      int num3 = 3;
+//        System.out.println(num3++);
+//        System.out.println(num3);
+//        System.out.println(++num3);
+//
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Choose a number: ");
+//        int userChoice = scanner. nextInt();
+//        System.out.println(userChoice);
+
+
+
+    }
+}
